a91364779da60ca5fc127b7905eb1826117139ac
L.D.R.N.GUNAWARDANA <nirashagunawardana9@gmail.com>
2024-08-29 12:09:13 +0530

    added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,38 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Hello and welcome!\n");
 
+        // Looping through numbers 1 to 5
         for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
+
+        // Calling a method to demonstrate basic calculations
+        demonstrateCalculations();
+
+        // Displaying a farewell message
+        System.out.println("Thank you for running the program. Goodbye!");
+    }
+
+    // A method to demonstrate basic calculations
+    private static void demonstrateCalculations() {
+        int a = 10;
+        int b = 5;
+
+        // Basic arithmetic operations
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+        int quotient = a / b;
+
+        // Displaying the results of calculations
+        System.out.println("\nBasic Calculations:");
+        System.out.println("a = " + a + ", b = " + b);
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Quotient: " + quotient);
     }
 }
